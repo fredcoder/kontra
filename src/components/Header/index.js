01708a46fc7e8eaec04e97a3c954bda898147583
Freddy <freddy.clavijo@gmail.com>
2021-09-20 10:36:43 +1000

    Refactor: Home Page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,10 +3,23 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import LocalMallRoundedIcon from '@material-ui/icons/LocalMallRounded';
 import SearchIcon from '@material-ui/icons/Search';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
 
+    state = {
+        isExpanded: false
+    }
+
+    toggleMenu = () => {
+        let { isExpanded } = this.state;
+        let newExpanded = isExpanded ? false : true
+        this.setState({ isExpanded: newExpanded });
+        console.log("newExpanded: ",newExpanded);
+    }
+
     render() {
+        let { isExpanded } = this.state;
 
         return (<header>
             <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
@@ -29,13 +42,22 @@ class Header extends Component {
                             <span className="navbar-toggler-icon"></span>
                         </button> */}
                     <div id="hamburguer-menu"
-                        className=" collapsed"
-                        data-bs-toggle="collapse"
+                        className={isExpanded ? " open" : " collapsed"}
+                        // data-bs-toggle="collapse"
+                        // data-bs-target="#navbarCollapse"
+                        // aria-controls="navbarCollapse"
+                        // aria-expanded="false"
+                        // aria-label="Toggle navigation"
+                        data-bs-toggle="collapsed"
                         data-bs-target="#navbarCollapse"
                         aria-controls="navbarCollapse"
-                        aria-expanded="false"
+                        aria-expanded={isExpanded}
                         aria-label="Toggle navigation"
-                        onClick={({ target }) => { target.classList.toggle('open') }}
+                        onClick={({ target }) => { 
+                             target.classList.toggle('open'); 
+                            console.log("target: ", target); 
+                            this.toggleMenu()
+                        }}
                     >
                         <span></span>
                         <span></span>
@@ -48,22 +70,22 @@ class Header extends Component {
                             <button className="btn btn-outline-info" type="submit">Search</button>
                         </form>
                     </div>
-                    <div className="navbar-collapse collapse" id="navbarCollapse" >
-                        <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                    <div className={`navbar-collapse collapse ${isExpanded ? "show" : ""}`} id="navbarCollapse" >
+                        <ul className="navbar-nav me-auto mb-2 mb-md-0" onClick={this.toggleMenu}>
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                <Link to="/Home" className="nav-link active" aria-current="page">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/Women">Women</a>
+                                <Link to="/Women" className="nav-link" >Women</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/Men">Men</a>
+                                <Link to="/Men" className="nav-link" >Men</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/Kids">Kids</a>
+                                <Link to="/Kids" className="nav-link" >Kids</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/Accessories">Accessories</a>
+                                <Link to="/Accessories" className="nav-link" >Accessories</Link>
                             </li>
                         </ul>
                     </div>
